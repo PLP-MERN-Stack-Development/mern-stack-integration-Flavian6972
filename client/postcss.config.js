588eefbfaf1577b7d954ce107c2bc7f1436e8a5a
Flavian6972 <onyangofle@gmail.com>
2025-11-09 00:44:22 +0300

    Add PostCSS configuration for client styling

---
+++ client/postcss.config.js
@@ -0,0 +1,17 @@
+// postcss.config.js
+/*export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};*/
+
+import tailwindcss from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
+
+export default {
+  plugins: [tailwindcss(), autoprefixer()],
+};
+
+
+
